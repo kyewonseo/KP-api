ec189cad134b72d2831ab8950c6666655b2addb1
kyewon <k@bluehack.net>
2017-02-16 23:36:50 +0900

    [KIOSK-11] login api lambda 구현

--- function/functions/login/index.js
+++ function/functions/login/index.js
@@ -1,7 +1,39 @@
-'use strict'
-var file = require('./login.json');
+'use strict';
 
-exports.handle = function(event, context, cb) {
-  console.log('processing event: %j', event.bmod);
-  cb(null, file);
-}
+const mysql = require('mysql');
+const db = require('./db/dbpool.js');
+const LoginRes = [];
+const resMsg = {};
+
+exports.handle = function (event, context, cb) {
+    console.log('processing event.user_id: %j', event.user_id);
+
+    const pool = db.create();
+
+    pool.getConnection(function (err, connection) {
+        connection.query("SELECT * FROM user WHERE u_id = " + event.user_id, function (err, data) {
+
+            if (err) {
+
+                connection.release();
+
+                resMsg ["responseStatus"] = "400";
+                resMsg ["responseMsg"] = "fail";
+                LoginRes.push(resMsg);
+
+                context.fail(JSON.stringify(LoginRes[0]));
+            }
+
+            connection.release();
+
+            resMsg ["responseStatus"] = "200";
+            resMsg ["responseMsg"] = "success";
+            resMsg ["data"] = data[0];
+
+            LoginRes.push(resMsg);
+            console.log('user info : ' + JSON.stringify(LoginRes[0]));
+
+            context.succeed(JSON.stringify(LoginRes[0]));
+        });
+    });
+};
