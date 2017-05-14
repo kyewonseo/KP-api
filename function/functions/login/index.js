4a99906138f38cded98aa0daed5542f09ea3989c
kyewon2 <kyewon76@gmail.com>
2017-05-14 15:13:17 +0900

    [kiosk-11] order history detail API (tax추가)

--- function/functions/login/index.js
+++ function/functions/login/index.js
@@ -1,52 +1,7 @@
-'use strict';
+'use strict'
+var file = require('./login.json');
 
-const mysql = require('mysql');
-const db = require('./db/dbpool.js');
-const LoginRes = [];
-const resMsg = {};
-
-exports.handle = function (event, context, cb) {
+exports.handle = function(event, context, cb) {
     console.log('processing event: %j', event);
-
-    const pool = db.create();
-
-    //TODO: fixme => passwd encrypt , decrypt
-    pool.getConnection(function (err, connection) {
-
-        connection.query("SELECT a.account_id, a.name, a.phone, a.token, a.status, a.regidate, " +
-            "u.user_id, u.store_id, u.u_points, u.u_status, u.u_level, u.regidate FROM account a " +
-            "INNER JOIN user u ON(u.account_id = a.account_id) " +
-            "WHERE a.account_id = ? AND a.passwd = ?", [event.login_id, event.login_passwd], function (err, rows) {
-
-            if (err) {
-
-                connection.release();
-
-                resMsg ["responseStatus"] = "400";
-                resMsg ["responseMsg"] = "login info is incorrect";
-                LoginRes.push(resMsg);
-
-                context.fail(LoginRes[0]);
-            }
-
-            connection.release();
-
-            if (rows[0] == null || rows[0] == "undefined") {
-                resMsg ["responseStatus"] = "400";
-                resMsg ["responseMsg"] = "rows is null";
-                LoginRes.push(resMsg);
-
-                context.fail(LoginRes[0]);
-            }else {
-                resMsg ["responseStatus"] = "200";
-                resMsg ["responseMsg"] = "success";
-                resMsg ["data"] = rows;
-
-                LoginRes.push(resMsg);
-                console.log('user info : ' + JSON.stringify(LoginRes[0]));
-
-                context.succeed(LoginRes[0]);
-            }
-        });
-    });
-};
+    cb(null, file);
+}
